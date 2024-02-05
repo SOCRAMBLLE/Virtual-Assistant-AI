91de3413286c8c514375345bd5329ca795acaf0e
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2024-02-05 17:25:55 +0000

    teste1

---
+++ backend1/target/debug/deps/backend1-95a3e97e4a885666.d
@@ -0,0 +1,5 @@
+C:\Users\foraa\Programacao\Projetos Pessoais\Virtual-Assistant-AI\backend1\target\debug\deps\libbackend1-95a3e97e4a885666.rmeta: src\main.rs
+
+C:\Users\foraa\Programacao\Projetos Pessoais\Virtual-Assistant-AI\backend1\target\debug\deps\backend1-95a3e97e4a885666.d: src\main.rs
+
+src\main.rs:
