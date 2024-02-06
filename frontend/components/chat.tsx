6c81dd018f498656a35eddf243986dbadeeb5524
SOCRAMBLLE <mfragaprt@me.com>
2024-02-06 17:02:37 +0100

    added base components for a chat page

---
+++ frontend/components/chat.tsx
@@ -0,0 +1,12 @@
+import "./chat.css"
+
+export const Chat = () => {
+    return (
+        <>
+            <div className="chat-container">
+                <h2>CHATBOX</h2>
+                
+            </div>
+        </>
+    )
+}
