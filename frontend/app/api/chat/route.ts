12a3fd74f95867e295c9ad3269b7d0dd080f2ef5
SOCRAMBLLE <mfragaprt@me.com>
2024-02-12 14:28:03 +0100

    Added route to send the input message from user

---
+++ frontend/app/api/chat/route.ts
@@ -0,0 +1,33 @@
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+export async function POST(req: Request) {
+  const json = await req.json();
+  const { message } = json;
+  const messageText = JSON.stringify(message)
+  const payload = {
+    role: "user",
+    content: messageText,
+  };
+  
+    const apiResponse = await fetch("https://backend-virtual-ai.onrender.com/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!apiResponse.ok) {
+    return new Response('HTTP error! Status:', { status: apiResponse.status } );
+  }
+
+  const data = await apiResponse.json();
+  const completion = data.data.content.trim()
+  console.log(completion)
+  
+  return new Response(completion, {
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+}
--- frontend/components/chat-input.tsx
+++ frontend/components/chat-input.tsx
@@ -1,29 +1,61 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import "./chat.css";
 
-export const ChatInput = () => {
-    const [message, setMessage] = useState("");
-    const [textareaHeight, setTextareaHeight] = useState("auto");
 
-    const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setMessage(event.target.value);
-        const textarea = event.target;  
-        textarea.style.height = '0';  
-        const newHeight = textarea.scrollHeight;  
-        const maxHeight = 110;
-        if (newHeight > maxHeight) {
-          textarea.style.height = `${maxHeight}px`;
-          setTextareaHeight(`${maxHeight}px`);
-        } else {
-          textarea.style.height = `${newHeight}px`;
-          setTextareaHeight(`${newHeight}px`);
-        }
-      };
+export const ChatInput = () => {
+  const [message, setMessage] = useState("");
+  const [textareaHeight, setTextareaHeight] = useState("auto");
+
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMessage(event.target.value);
+    const textarea = event.target;
+    textarea.style.height = "0";
+    const newHeight = textarea.scrollHeight;
+    const maxHeight = 110;
+    if (newHeight > maxHeight) {
+      textarea.style.height = `${maxHeight}px`;
+      setTextareaHeight(`${maxHeight}px`);
+    } else {
+      textarea.style.height = `${newHeight}px`;
+      setTextareaHeight(`${newHeight}px`);
+    }
+  };
+
+  const handleInputSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/chat', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to fetch, status: ${response.status}`);
+      }
+      const data = await response.text();
+      console.log("Mensagem enviada:", message);
+      console.log("Resposta:", data);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  
+  };
+
   return (
-    <form className="input-container">
-      <textarea onChange={handleTextareaChange} rows={1} cols={50} style={{ height: textareaHeight }} value={message}/>
+    <form className="input-container" onSubmit={handleInputSubmit}>
+      <textarea
+        onChange={handleTextareaChange}
+        rows={1}
+        cols={50}
+        style={{ height: textareaHeight }}
+        value={message}
+      />
       <button type="submit">Send</button>
     </form>
   );
